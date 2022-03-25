818b4d46c9ffe53b8c8f8d96970249e766dc315f
Alexander Tegelid <alex@humblegiant.se>
2022-03-25 23:24:03 +0100

    Add slash to layer 4

--- keyboards/ferris/keymaps/twominds/keymap.c
+++ keyboards/ferris/keymaps/twominds/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TRNS, KC_TRNS,                                                   KC_TRNS, KC_TRNS
 	),
 	[4] = LAYOUT_split_3x5_2(
-		KC_TRNS, SE_COLN, SE_LABK, SE_RABK, SE_SCLN,                        KC_TRNS, KC_TRNS, KC_TRNS, SE_ARNG, SE_ADIA,
+		SE_SLSH, SE_COLN, SE_LABK, SE_RABK, SE_SCLN,                        KC_TRNS, KC_TRNS, KC_TRNS, SE_ARNG, SE_ADIA,
 		SE_LCBR, SE_RCBR, SE_LPRN, SE_RPRN, SE_AT,                          KC_TRNS, KC_NO, SE_EQL, SE_PLUS, SE_PERC,
 		KC_TRNS, SE_EXLM, SE_LBRC, SE_RBRC, SE_QUES,                        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_VOLD,                                                   KC_VOLU, KC_TRNS
