7711dd6020cb321e9a41c8f7e155a09632bf0291
Alexander Tegelid <alex@humblegiant.se>
2022-05-17 16:13:07 +0200

    Friendly layer names

--- keyboards/ferris/keymaps/twominds/keymap.c
+++ keyboards/ferris/keymaps/twominds/keymap.c
@@ -5,6 +5,20 @@
 
 #include "capsword.c"
 
+// Friendly layer names
+enum layers {
+  BASE,
+  MOUSE,
+  NAV,
+  SYMB1,
+  SYMB2,
+  FUN,
+  NUM,
+  NAV2,
+  GAME,
+};
+
+
 // Win and tab switches
 #define TABL LAG(KC_LEFT)
 #define TABR LAG(KC_RGHT)
@@ -27,59 +41,59 @@ enum keycodes {
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT_split_3x5_2(
-		KC_Q,           KC_W,         KC_E,           KC_R,       KC_T,               KC_Y,    KC_U,       KC_I,            KC_O,           KC_P,
-		LSFT_T(KC_A),   LT(5,KC_S),   LT(1,KC_D),     LT(3,KC_F), KC_G,               KC_H,    LT(4,KC_J), LT(2,KC_K),      LT(6,KC_L),     LSFT_T(KC_SCLN),
-		LCAG_T(KC_Z),   LCTL_T(KC_X), LALT_T(KC_C),   KC_V,       KC_B,               KC_N,    KC_M,       LALT_T(KC_COMM), LCTL_T(KC_DOT), KC_SLSH,
-													  LTHUMB1,    LTHUMB2,            RTHUMB1, RTHUMB2
+	[BASE] = LAYOUT_split_3x5_2(
+		KC_Q,         KC_W,         KC_E,           KC_R,           KC_T,               KC_Y,    KC_U,           KC_I,            KC_O,           KC_P,
+		LSFT_T(KC_A), LT(FUN,KC_S), LT(MOUSE,KC_D), LT(SYMB1,KC_F), KC_G,               KC_H,    LT(SYMB2,KC_J), LT(NAV,KC_K),    LT(NUM,KC_L),   LSFT_T(KC_SCLN),
+		LCAG_T(KC_Z), LCTL_T(KC_X), LALT_T(KC_C),   KC_V,           KC_B,               KC_N,    KC_M,           LALT_T(KC_COMM), LCTL_T(KC_DOT), KC_SLSH,
+													LTHUMB1,        LTHUMB2,            RTHUMB1, RTHUMB2
 	),
-	[1] = LAYOUT_split_3x5_2(
+	[MOUSE] = LAYOUT_split_3x5_2(
 		_______, _______, _______, _______, _______,                        _______, KC_BTN1, KC_MS_U, KC_BTN2, _______,
 		_______, KC_BTN1, XXXXXXX, KC_BTN2, _______,                        _______, KC_MS_L, KC_MS_D, KC_MS_R, _______,
 		_______, _______, _______, _______, _______,                        KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R, _______,
 								   _______, _______,                        _______, _______
 	),
-	[2] = LAYOUT_split_3x5_2(
+	[NAV] = LAYOUT_split_3x5_2(
 		_______, _______, KC_UP,   _______, _______,                        _______, _______, _______, _______,       _______,
 		_______, KC_LEFT, KC_DOWN, KC_RGHT, KC_C,                           _______, KC_LGUI, XXXXXXX, LCTL(KC_LALT), LSA(KC_LSFT),
 		_______, KC_PGUP, KC_PGDN, KC_HOME, KC_END,                         _______, _______, _______, _______,       _______,
 								   _______, _______,                        _______, _______
 	),
-	[3] = LAYOUT_split_3x5_2(
+	[SYMB1] = LAYOUT_split_3x5_2(
 		_______, _______, _______, _______, _______,                        _______, SE_UNDS, SE_PIPE, SE_QUOT, _______,
 		SE_CIRC, SE_ASTR, SE_AMPR, XXXXXXX, _______,                        SE_HASH, SE_TILD, SE_SLSH, SE_DQUO, SE_DLR,
 		_______, _______, _______, _______, _______,                        _______, SE_MINS, SE_BSLS, SE_GRV,  _______,
 								   _______, _______,                        _______, _______
 	),
-	[4] = LAYOUT_split_3x5_2(
+	[SYMB2] = LAYOUT_split_3x5_2(
 		SE_SLSH, SE_COLN, SE_LABK, SE_RABK, SE_SCLN,                        _______, _______, _______, SE_ARNG, SE_ADIA,
 		SE_LCBR, SE_RCBR, SE_LPRN, SE_RPRN, SE_AT,                          _______, XXXXXXX, SE_EQL,  SE_PLUS, SE_PERC,
 		_______, SE_EXLM, SE_LBRC, SE_RBRC, SE_QUES,                        _______, _______, _______, _______, _______,
 								   _______, _______,                        KC_VOLD, KC_VOLU
 	),
-	[5] = LAYOUT_split_3x5_2(
+	[FUN] = LAYOUT_split_3x5_2(
 		_______, _______, TABL,          TABR,    _______,                  _______, KC_F7, KC_F8, KC_F9, KC_F10,
 		_______, XXXXXXX, LCTL(KC_LALT), SW_APP,  _______,                  _______, KC_F4, KC_F5, KC_F6, KC_F11,
 		_______, _______, PREVWIN,       NEXTWIN, _______,                  _______, KC_F1, KC_F2, KC_F3, KC_F12,
 										 _______, _______,                  _______, _______
 	),
-	[6] = LAYOUT_split_3x5_2(
+	[NUM] = LAYOUT_split_3x5_2(
 		KC_PSLS, KC_7, KC_8, KC_9,    KC_PPLS,                              _______, _______, _______, _______, _______,
 		KC_0,    KC_4, KC_5, KC_6,    KC_PMNS,                              _______, _______, _______, XXXXXXX, _______,
 		KC_PAST, KC_1, KC_2, KC_3,    KC_PEQL,                              _______, _______, _______, _______, _______,
 							 _______, _______,                              _______, _______
 	),
-	[7] = LAYOUT_split_3x5_2(
-		_______, _______, SE_COLN, KC_ESC,   _______,                       DF(8),   _______, QK_BOOT, _______, KC_DEL,
-		_______, SE_PERC, SE_SLSH, KC_ENT,   _______,                       DF(1),   KC_LGUI, _______, _______, _______,
-		_______, _______, SE_QUES, SE_EXLM,  _______,                       DF(0),   _______, _______, _______, _______,
-								   CAPSWORD, KC_TAB,                        XXXXXXX, _______
+	[NAV2] = LAYOUT_split_3x5_2(
+		_______, _______, SE_COLN, KC_ESC,   _______,                       DF(GAME),  _______, QK_BOOT, _______, KC_DEL,
+		_______, SE_PERC, SE_SLSH, KC_ENT,   _______,                       DF(MOUSE), KC_LGUI, _______, _______, _______,
+		_______, _______, SE_QUES, SE_EXLM,  _______,                       DF(BASE),  _______, _______, _______, _______,
+								   CAPSWORD, KC_TAB,                        XXXXXXX,   _______
 	),
-	[8] = LAYOUT_split_3x5_2(
-		KC_ESC,  KC_1,   KC_2, KC_3,    KC_4,                               KC_5,    KC_6,    KC_7,    KC_8,    KC_9,
-		XXXXXXX, KC_Q,   KC_W, KC_E,    KC_R,                               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-		KC_LSFT, KC_A,   KC_S, KC_D,    KC_F,                               KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,
-							   KC_ESC,  KC_SPC,                             DF(0),   XXXXXXX
+	[GAME] = LAYOUT_split_3x5_2(
+		KC_ESC,  KC_1,   KC_2, KC_3,    KC_4,                               KC_5,     KC_6,    KC_7,    KC_8,    KC_9,
+		XXXXXXX, KC_Q,   KC_W, KC_E,    KC_R,                               XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+		KC_LSFT, KC_A,   KC_S, KC_D,    KC_F,                               KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,
+							   KC_ESC,  KC_SPC,                             DF(BASE), XXXXXXX
 	)
 };
 
