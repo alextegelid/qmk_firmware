6e3d907dbc3766b21fe2af2e5f1617ff46c05c85
Alexander Tegelid <alex@humblegiant.se>
2022-08-10 20:23:41 +0200

    Fix more indentation

--- keyboards/ferris/keymaps/twominds/swapper.h
+++ keyboards/ferris/keymaps/twominds/swapper.h
@@ -11,10 +11,10 @@
 //
 // This behaviour is useful for more than just cmd-tab, hence: cmdish, tabish.
 void update_swapper(
-    bool *active,
-    uint16_t cmdish,
-    uint16_t tabish,
-    uint16_t trigger,
-    uint16_t keycode,
-    keyrecord_t *record
+  bool *active,
+  uint16_t cmdish,
+  uint16_t tabish,
+  uint16_t trigger,
+  uint16_t keycode,
+  keyrecord_t *record
 );
