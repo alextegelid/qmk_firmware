eaceb4cf7efefdab38ffeaac410c53545d707af0
Alexander Tegelid <alex@humblegiant.se>
2022-08-23 10:45:40 +0200

    Add tab to gaming layer

--- keyboards/ferris/keymaps/twominds/keymap.c
+++ keyboards/ferris/keymaps/twominds/keymap.c
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [GAME] = LAYOUT_split_3x5_2(
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,            KC_5,     KC_6,    KC_7,   KC_8,    KC_9,
-    ___X___, KC_Q,    KC_W,    KC_E,    KC_R,            KC_TAB,   ___X___, KC_G,   KC_B,    MUTEDISC,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,            KC_TAB,   ___X___, KC_G,   KC_B,    MUTEDISC,
     KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,            KC_F1,    KC_F2,   KC_F3,  KC_F4,   KC_F5,
                                KC_ESC,  KC_SPC,          DF(QWER), ___X___
   )
