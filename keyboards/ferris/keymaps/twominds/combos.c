ff3b5a8b53e9e8031c663c87bd361c83eb441a31
Alexander Tegelid <alex@humblegiant.se>
2023-02-05 14:38:17 +0100

    Remove escape and enter combos

--- keyboards/ferris/keymaps/twominds/combos.c
+++ keyboards/ferris/keymaps/twominds/combos.c
@@ -1,16 +1,10 @@
 enum combos {
-  QWER_ESC,
-  QWER_ENT,
   QWER_BOOT,
 };
 
-const uint16_t PROGMEM qwer_esc[] = {KC_E, KC_R, COMBO_END};
-const uint16_t PROGMEM qwer_ent[] = {KC_U, KC_I, COMBO_END};
 const uint16_t PROGMEM qwer_boot[] = {KC_V, KC_B, KC_N, KC_M, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
-  [QWER_ESC] = COMBO(qwer_esc, KC_ESC),
-  [QWER_ENT] = COMBO(qwer_ent, KC_ENT),
   [QWER_BOOT] = COMBO(qwer_boot, QK_BOOT),
 };
 
@@ -19,8 +13,6 @@ combo_t key_combos[COMBO_COUNT] = {
 bool get_combo_must_hold(uint16_t index, combo_t *combo) {
   switch(index) {
     case QWER_BOOT:
-    case QWER_ESC:
-    case QWER_ENT:
       return true;
     default:
       return false;
