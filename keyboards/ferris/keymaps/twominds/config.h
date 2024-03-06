df14ee574bd78964b2e398b855fbd82b1a8a90b6
Alexander Tegelid <alex@humblegiant.se>
2024-03-06 18:33:39 +0100

    Show white when layer is toggled

--- keyboards/ferris/keymaps/twominds/config.h
+++ keyboards/ferris/keymaps/twominds/config.h
@@ -66,5 +66,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   #define RGBLIGHT_DEFAULT_SAT 255
   #define RGBLIGHT_DEFAULT_VAL RGBLIGHT_LIMIT_VAL
 
+  #define RGBLIGHT_LAYERS
+
   #define RGBLIGHT_SLEEP
 #endif
