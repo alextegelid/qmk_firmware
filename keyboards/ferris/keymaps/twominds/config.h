d767294d1f1d7c186e3cc5e0bb480b1b9434c5a5
Alexander Tegelid <alex@humblegiant.se>
2024-03-08 07:39:02 +0100

    Disable quick tap

--- keyboards/ferris/keymaps/twominds/config.h
+++ keyboards/ferris/keymaps/twominds/config.h
@@ -37,7 +37,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Pick good defaults for enabling homerow modifiers
 #define TAPPING_TERM 200
 #define PERMISSIVE_HOLD
-#define TAPPING_FORCE_HOLD
+#define QUICK_TAP_TERM 0
 
 // Combo key configuration
 #define COMBO_COUNT 3
